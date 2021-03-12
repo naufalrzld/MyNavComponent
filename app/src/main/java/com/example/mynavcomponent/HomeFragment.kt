cfb4e3527ebc2c3a0c9bff8da73f1323a0167ec6
Naufal <naufalrzld@gmail.com>
2021-03-12 17:18:44 +0700

    rename nav graph & remove unused code

--- app/src/main/java/com/example/mynavcomponent/HomeFragment.kt
+++ app/src/main/java/com/example/mynavcomponent/HomeFragment.kt
@@ -1,13 +1,11 @@
 package com.example.mynavcomponent
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
-import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
 class HomeFragment : Fragment() {
